ac0dd36cb4c4c8dc08b93a313362b6124ff7fbfd
Damian Romanowski <romfir22@gmail.com>
2021-02-26 04:31:33 +0100

    Revert "removed static urls"
    This reverts commit 387b53a2d8e1d91b4713b5356f65a2b6940d76b9.

---
+++ HtmlRaportGenerator/Shared/RelativePageUrls.cs
@@ -0,0 +1,9 @@
+﻿namespace HtmlRaportGenerator.Shared
+{
+    public static class RelativePageUrls
+    {
+        public const string MainView = "";
+
+        public const string MonthEdit = "MonthEdit";
+    }
+}
