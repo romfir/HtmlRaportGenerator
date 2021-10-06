1ff907f63aaa6d8351a7a9bc3e6d7eb258a5bb68
romfir <33402460+romfir@users.noreply.github.com>
2021-10-06 15:30:39 +0000

    Deploying to gh-pages from @ romfir/HtmlRaportGenerator@8e4c6bc6f5a9341daaa56d480fd161f849e89a8e 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -53,7 +53,7 @@
       "url": "_framework\/icudt.dat"
     },
     {
-      "hash": "sha256-ruIxfOnAY608MJan8TDv9go\/OVRqn3OWKFRXxDZSkQk=",
+      "hash": "sha256-ALUwiTvWyaMrMIifvHbXSwJWSmhuUT\/xtzEolhGdJJc=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -89,7 +89,7 @@
       "url": "_framework\/Blazorise.Icons.FontAwesome.dll"
     },
     {
-      "hash": "sha256-a5b187zRohETPuKO+YuAQ2SCP6e7dggEqVAi5F5DZqo=",
+      "hash": "sha256-I02Hw0Eioh6J2\/Yii7GMcQQhz1Gat+NRR7DXLf2OJhM=",
       "url": "_framework\/HtmlRaportGenerator.dll"
     },
     {
@@ -421,5 +421,5 @@
       "url": "_content\/Microsoft.AspNetCore.Components.WebAssembly.Authentication\/AuthenticationService.js.LICENSE.txt"
     }
   ],
-  "version": "fHeACwJB"
+  "version": "BeoBQiZn"
 };
--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: fHeACwJB */
+/* Manifest version: BeoBQiZn */
